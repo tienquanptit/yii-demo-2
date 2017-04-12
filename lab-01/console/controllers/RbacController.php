428c90d8b8046bcc6809061618c5ba308cfafbcb
QuanTien <vinhnt9x@gmail.com>
2017-04-12 13:22:04 +0700

    commit update file main.php

--- lab-01/console/controllers/RbacController.php
+++ lab-01/console/controllers/RbacController.php
@@ -17,22 +17,53 @@
 //            $createPost = $auth->createPermission('create-post');
 //            $createPost->description = 'Creat a post';
 //            $auth->add($createPost);
+//
+//            $indexPost = $auth->createPermission('index-post');
+//            $indexPost->description = 'Xem danh sach bai viet';
+//            $auth->add($indexPost);
+//
+//            $updatePost = $auth->createPermission('update-post');
+//            $updatePost->description = 'Chinh sua bai viet';
+//            $auth->add($updatePost);
+//
+//            $viewPost = $auth->createPermission('view-post');
+//            $viewPost->description = 'Xem chi tiet bai viet';
+//            $auth->add($viewPost);
+//
+//            $deletePost = $auth->createPermission('delete-post');
+//            $deletePost->description = 'Xoa bai viet';
+//            $auth->add($deletePost);
 
-            $indexPost = $auth->createPermission('index-post');
-            $indexPost->description = 'Xem danh sach bai viet';
-            $auth->add($indexPost);
 
-            $updatePost = $auth->createPermission('update-post');
-            $updatePost->description = 'Chinh sua bai viet';
-            $auth->add($updatePost);
 
-            $viewPost = $auth->createPermission('view-post');
-            $viewPost->description = 'Xem chi tiet bai viet';
-            $auth->add($viewPost);
+//            $postManager = $auth->createRole('manager-post');
+//            $postManager->description ='Post Manager';
+//            $auth->add($postManager);
 
-            $deletePost = $auth->createPermission('delete-post');
-            $deletePost->description = 'Xoa bai viet';
-            $auth->add($deletePost);
+
+//            $createPost = $auth->createPermission('create-post');
+//
+//            $indexPost = $auth->createPermission('index-post');
+//
+//            $updatePost = $auth->createPermission('update-post');
+//
+//            $viewPost = $auth->createPermission('view-post');
+//
+//            $deletePost = $auth->createPermission('delete-post');
+
+//            $postManager = $auth->createRole('manager-post');
+//
+            $admin = $auth->createRole('admin');
+//            $auth->add($admin);
+//
+//            $auth->addChild($admin, $postManager);
+//            $auth->addChild($postManager, $createPost);
+//            $auth->addChild($postManager, $indexPost);
+//            $auth->addChild($postManager, $updatePost);
+//            $auth->addChild($postManager, $viewPost);
+//            $auth->addChild($postManager, $deletePost);
+
+            $auth->assign($admin,1);
         }
     }
 ?>
